f310b1ff1ed9da6b77f49e99a4b03e8aa3bfae5f
Jonathan Kowa <jonathan@kowa.io>
2020-04-17 12:48:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,21 +3,47 @@ package main
 import (
 	"fmt"
 	"github.com/fuzxxl/nfc/2.0/nfc"
+	"log"
 )
 
 var m = nfc.Modulation{Type: nfc.ISO14443a}
+var devstr = ""
 
 func main(){
-	m := nfc.Modulation{Type: nfc.ISO14443a}
-	devices,_ := nfc.ListDevices()
-	dev := nfc.Device{}
-	pnd, err := nfc.Open("")
-	if err != nil {
-		fmt.Println("Could not open device.")
-	}
-	fmt.Println("bis hier")
-	fmt.Println(devices)
+	//m := nfc.Modulation{Type: nfc.ISO14443a}
+	//devices,_ := nfc.ListDevices()
+	//dev := nfc.Device{}
+	//pnd, err := nfc.Open("")
+	//if err != nil {
+	//	fmt.Println("Could not open device.")
+	//}
+	//fmt.Println("bis hier")
+	//fmt.Println(devices)
 
+	fmt.Println("using libnfc", nfc.Version())
+
+	pnd, err := nfc.Open(devstr)
+	if err != nil {
+		log.Fatalf("could not open device: %v", err)
+	}
+	defer pnd.Close()
+
+	if err := pnd.InitiatorInit(); err != nil {
+		log.Fatalf("could not init initiator: %v", err)
+	}
+
+	fmt.Println("opened device", pnd, pnd.Connection())
+
+	card_id, err := get_card(&pnd)
+	if err != nil {
+		fmt.Errorf("failed to get_card", err)
+	}
+
+	if card_id != [10]byte{} {
+		fmt.Printf("card found %#X\n", card_id)
+	} else {
+		fmt.Printf("no card found\n")
+	}
 }
 
 func get_card (pnd *nfc.Device) ([10]byte, error) {

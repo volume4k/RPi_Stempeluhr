273606047bd09660ba0f8f5a379d3f7082dd2f4b
Jonathan Kowa <jonathan@kowa.io>
2020-04-17 12:44:53 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"github.com/fuzxxl/nfc/2.0/nfc"
 )
 
+var m = nfc.Modulation{Type: nfc.ISO14443a}
+
 func main(){
+	m := nfc.Modulation{Type: nfc.ISO14443a}
 	devices,_ := nfc.ListDevices()
 	dev := nfc.Device{}
 	pnd, err := nfc.Open("")
@@ -14,6 +17,20 @@ func main(){
 	}
 	fmt.Println("bis hier")
 	fmt.Println(devices)
-	fmt.Println(dev)
-	fmt.Println(pnd)
+
+}
+
+func get_card (pnd *nfc.Device) ([10]byte, error) {
+	for {
+		targets, err := pnd.InitiatorListPassiveTargets(m)
+		if err != nil {
+			return [10]byte{}, fmt.Errorf("listing available nfc targets", err)
+		}
+
+		for _, t := range targets {
+			if card, ok := t.(*nfc.ISO14443aTarget); ok {
+				return card.UID, nil
+			}
+		}
+	}
 }

398b074af02db5a2efee6744c6526b9519d6e961
Jonathan Kowa <jonathan@kowa.io>
2020-04-17 11:38:41 +0200

    Update nfc.go

--- nfc.go
+++ nfc.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"periph.io/x/periph/experimental/devices/mfrc522"
+	"periph.io/x/periph/experimental/devices/mfrc522/commands"
 	"time"
 )
 
 
 func readTag(){
 	fmt.Println("hi")
-	var auth byte = 0
+	auth:= byte(commands.PICC_AUTHENT1B)
 	key := mfrc522.DefaultKey
 	dev := mfrc522.Dev{}
 	uid, err := dev.ReadCard(time.Second*15, auth, 0, 0, key)
